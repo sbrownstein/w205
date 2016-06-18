a17a3a181bad8f1a5a08cc169d3225b803a2973b
S. Brownstein <smarx@berkeley.edu>
2016-06-17 22:55:58 -0400

    Modified command lines

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,13 +1,9 @@
-mv Hospital\ General\ Information.csv hospitals.csv
+Commands for renaming files:
 mv Timely\ and\ Effective\ Care\ -\ Hospital.csv effective_care.csv
-mv Readmissions\ and\ Deaths\ -\ Hospital.csv readmissions.csv
-mv Measure\ Dates.csv Measures.csv
 mv hvbp_hcahps_05_28_2015.csv surveys_responses.csv
 
-
+Commands for loading files into HDFS:
 hdfs dfs -mkdir /user/w205/hospital_compare
 hdfs dfs -put effective_care.csv /user/w205/hospital_compare
-hdfs dfs -put hospitals.csv /user/w205/hospital_compare
-hdfs dfs -put measures.csv /user/w205/hospital_compare
-hdfs dfs -put readmissions.csv /user/w205/hospital_compare
 hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare
+
