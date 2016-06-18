5ac1c72c19b9572f666218a5d2f23f178ec1cffb
sbrownstein <smarx@berkeley.edu>
2016-06-18 09:35:28 -0400

    Update load_data_lake.sh

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,7 +1,8 @@
 mv Timely\ and\ Effective\ Care\ -\ Hospital.csv effective_care.csv
-mv hvbp_hcahps_05_28_2015.csv surveys_responses.csv
+mv hvbp_hcahps_05_28_2015.csv survey_responses.csv
+mv hvbp_tps_05_28_2015.csv total_performance.csv
 
 hdfs dfs -mkdir /user/w205/hospital_compare
 hdfs dfs -put effective_care.csv /user/w205/hospital_compare
-hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare
-
+hdfs dfs -put survey_responses.csv /user/w205/hospital_compare
+hdfs dfs -put total_performance.csv /user/w205/hospital_compare
