b4dd69ce8604db3029997da5726f61c47f706f42
sbrownstein <smarx@berkeley.edu>
2016-06-17 22:57:48 -0400

    Update load_data_lake.sh

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,8 +1,6 @@
-Commands for renaming files:
 mv Timely\ and\ Effective\ Care\ -\ Hospital.csv effective_care.csv
 mv hvbp_hcahps_05_28_2015.csv surveys_responses.csv
 
-Commands for loading files into HDFS:
 hdfs dfs -mkdir /user/w205/hospital_compare
 hdfs dfs -put effective_care.csv /user/w205/hospital_compare
 hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare
